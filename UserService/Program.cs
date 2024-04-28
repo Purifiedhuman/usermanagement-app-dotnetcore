96c3f50342e66385b051bddeb3d55a5c55e8d328
Tey Han Yee <teyhanyee@gmail.com>
2024-04-28 20:06:01 +0800

    chore: connection string name

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AZURE_POSTGRESQL_CONNECTIONSTRING"));
 });
 
 builder.Services.AddAuthorization();
